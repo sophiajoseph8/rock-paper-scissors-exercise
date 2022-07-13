438d2de6f793e0ae08023371f3349761e19377dc
sophiajoseph8 <sj1332@stern.nyu.edu>
2022-07-12 21:34:08 -0400

    Minor updates - adding lines between output

--- game.py
+++ game.py
@@ -2,8 +2,9 @@
 
 print("Rock, Paper, Scissors, Shoot!")
 
+print ("----------")
 print ("Welcome to the rock-paper-scissors game!")
-
+print ("----------")
 # player's choice
 player_choice = input("Please choose 'rock' paper' or 'scissors':")
 # standardize the input
@@ -27,28 +28,42 @@ computer_choice = (random.choice(arr))
 # standardize the input
 computer_choice = computer_choice.lower()
 print ("The computer chose:", (computer_choice))
+print ("----------")
 
 # determine the winner and display the results.
+# rock options
 if player_choice == "rock" and computer_choice == "rock":
     print ("Oh it's a tie!")
+    print ("----------")
 elif player_choice == "rock" and computer_choice == "paper":
     print ("Oh, the computer won. Maybe next time!")
+    print ("----------")
 elif player_choice == "rock" and computer_choice == "scissors":
     print ("Congratulations - you win!")
+    print ("----------")
+# paper
 elif player_choice == "paper" and computer_choice == "paper":
     print ("Oh it's a tie!")
+    print ("----------")
 elif player_choice == "paper" and computer_choice == "rock":
     print ("Congratuations - you win!")
+    print("----------")
 elif player_choice == "paper" and computer_choice == "scissors":
     print ("Oh, the computer won. Maybe next time!")
+    print ("----------")
+# scissors options
 elif player_choice == "scissors" and computer_choice == "scissors":
     print ("Oh it's a tie!")
+    print ("----------")
 elif player_choice == "scissors" and computer_choice == "rock":
     print ("Oh, the computer won. Maybe next time!")
+    print("----------")
 elif player_choice == "scissors" and computer_choice == "paper":
     print ("Congratulations - you win!")
+    print ("----------")
 else:
     print ("Sorry, please try again.")
+    print ("----------")
 
 
 # finish the game
