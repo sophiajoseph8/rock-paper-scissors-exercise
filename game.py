8aacf9f987d2b65c400663568978d5bef1e582f7
sophiajoseph8 <sj1332@stern.nyu.edu>
2022-07-12 20:02:50 -0400

    player choice and computer choice

--- game.py
+++ game.py
@@ -1,3 +1,30 @@
 # this is the "game.py" file...
 
 print("Rock, Paper, Scissors, Shoot!")
+
+print ("Welcome to the rock-paper-scissors game!")
+
+# player's choice
+player_choice = input("Please choose 'rock' paper' or 'scissors':")
+print ("You chose:", (player_choice))
+
+# validate user inputs
+
+# computer's choice
+
+import random
+random.random()
+arr = ["rock", "paper", "scissors"]
+computer_choice = (random.shuffle(arr))
+print ("The computer chose:", random.choice(arr))
+
+# determine the winner
+
+
+
+print ("Thanks for playing! Please play again.")
+# display the results
+
+
+
+
