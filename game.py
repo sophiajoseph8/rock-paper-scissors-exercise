d4298e7a75fab1b4dbcf0fa97b111de0a2395206
sophiajoseph8 <sj1332@stern.nyu.edu>
2022-07-12 21:08:00 -0400

    input validation, input standardization and winner determination

--- game.py
+++ game.py
@@ -6,30 +6,59 @@ print ("Welcome to the rock-paper-scissors game!")
 
 # player's choice
 player_choice = input("Please choose 'rock' paper' or 'scissors':")
+# standardize the input
+player_choice = player_choice.lower()
 print ("You chose:", (player_choice))
 
-# validate user inputs
-valid_options= {"rock_one": "Rock", "rock_two": "rock", "rock_three": "ROCK","paper_one": "Paper", "paper_two": "paper", "paper_three": "PAPER", "scissors_one":"Scissors", "scissors_two": "scissors", "scissors_three": "SCISSORS"}
+valid_options = ["rock", "paper", "scissors"]
 
-
-
-
-
+# if not in the list, terminate the game
+if player_choice not in valid_options:
+    print ("Oops - invalid selection. Please try again.")
+    exit()
 
 # computer's choice
 
 import random
 random.random()
 arr = ["rock", "paper", "scissors"]
-computer_choice = (random.shuffle(arr))
-print ("The computer chose:", random.choice(arr))
+computer_choice = (random.choice(arr))
+# standardize the input
+computer_choice = computer_choice.lower()
+print ("The computer chose:", (computer_choice))
 
 # determine the winner
+if player_choice == "rock" and computer_choice == "rock":
+    print ("Oh it's a tie!")
+elif player_choice == "rock" and computer_choice == "paper":
+    print ("Oh, the computer won. Maybe next time!")
+elif player_choice == "rock" and computer_choice == "scissors":
+    print ("Congratulations - you win!")
+elif player_choice == "paper" and computer_choice == "paper":
+    print ("Oh it's a tie!")
+elif player_choice == "paper" and computer_choice == "rock":
+    print ("Congratuations - you win!")
+elif player_choice == "paper" and computer_choice == "scissors":
+    print ("Oh, the computer won. Maybe next time!")
+elif player_choice == "scissors" and computer_choice == "scissors":
+    print ("Oh it's a tie!")
+elif player_choice == "scissors" and computer_choice == "rock":
+    print ("Oh, the computer won. Maybe next time!")
+elif player_choice == "scissors" and computer_choice == "paper":
+    print ("Congratulations - you win!")
+else:
+    print ("Sorry, please try again.")
+
+# validate user inputs
+breakpoint()
 
 
+
+
+# display the results
 
 print ("Thanks for playing! Please play again.")
-# display the results
+
 
 
 
